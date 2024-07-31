31bd3730a9f6420627ed5bd43a69e18c4b111781
triangle-park <silver3garden@gmail.com>
2024-07-31 11:00:26 +0900

    container 생성

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Landing = () => {
+  return (
+    <LandingContainer>
+        <ServiceProfileContainer></ServiceProfileContainer>
+        <FeatureContainer></FeatureContainer>
+        <TargetedContainer></TargetedContainer>
+        <SignUpLinkContainer></SignUpLinkContainer>
+    </LandingContainer>
+  );
+}
+
+export default Landing;
