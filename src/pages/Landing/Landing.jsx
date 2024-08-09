a48723f25f874cb5cc5ce1e66db742f5f238a0c2
triangle-park <silver3garden@gmail.com>
2024-08-09 17:24:11 +0900

    랜딩페이지 2차 완성

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -34,11 +34,13 @@ export default function Landing() {
                       </div>
                     }
             />
-            <div>
+            <div className='ContainerBox'>
                 <CreatorContainer>
                     <div className='TextContainer'>
-                        <h4>로컬 크리에이터</h4>
-                        <p>지역의 자연•문화 특성과 아이디어를 결합해 사업적 가치를 창출하는 창업가</p>
+                        <div>
+                            <h4>로컬 크리에이터</h4>
+                            <p>지역의 자연•문화 특성과 아이디어를 결합해 사업적 가치를 창출하는 창업가</p>
+                        </div>
                         <Link to="/signup?role=Creator">
                             <button>로컬 크리에이터로 시작하기</button>
                         </Link>
@@ -80,61 +82,71 @@ export default function Landing() {
                     </div>
                 </CreatorContainer>
                 <ConsumerContainer>
-                    <div className='ImageBox'>
-                        <img src={dodari} alt='dodari' />
-                        <img src={soonsoap} alt='soonsoap' />
+                    <div className='ImageBoxContainer'>
+                        <div className='ImageBox'>
+                            <img src={dodari} alt='dodari' />
+                        </div>
+                        <div className='ImageBox'>
+                            <img src={soonsoap} alt='soonsoap' />
+                        </div>
+                        <div className='TextBox'>
+                            <h4>직접 인증받은 브랜드만 입점</h4>
+                            <Link to="/signup?role=Consumer">
+                                <button>로컬 컨슈머로 시작하기</button>
+                            </Link>
+                        </div>
                     </div>
-                    <div className='TextBox'>
-                        <h4>직접 인증받은 브랜드만 입점</h4>
-                        <Link to="/signup?role=Consumer">
-                            <button>로컬 컨슈머로 시작하기</button>
-                        </Link>
-                    </div>
+                    
                 </ConsumerContainer>
             </div>
-            <FeatureContainer>
-                <img src={mockup2} alt="Mockup 2" />
-                <div>
-                    <p id="feature-title">로컬 비즈?��?�� ?���?</p>
-                    <p>
-                        로컬 비즈?��?�� ?���? ?��?��
-                        <br />
-                        로컬 비즈?��?�� ?���? ?��?��
-                        <br />
-                        로컬 비즈?��?�� ?���? ?��?��
-                    </p>
-                    <ul>
-                        <li>Benefit of Feature</li>
-                        <li>Benefit of Feature</li>
-                        <li>Benefit of Feature</li>
-                    </ul>
+            <BrandCoverContainer>
+                <div className='BrandCover'>
+                    <div className='ImageBox'>
+                        <img src={mockup3} alt='brand_cover_1' />
+                    </div>
+                    <div className='TextBox'>
+                        <div className='BrandNameBox'>
+                            <p className='BrandName'>스틸마스프링</p>
+                            <p className='BrandLocation'>세종특별자치시 조치원의 도예 공방</p>
+                        </div>
+                        <div className='InterviewBox'>
+                            <p className='Interview'>
+                                "청주는 공예비엔날레를 진행해서 세계에 있는 공예인들이 많이 방문하는 곳이에요.<br />
+                                ...요즘은 서울에 있는 동네들은 뜨기 시작하면 대부분 비슷해지는 경향이 있는 것 같아요.<br />
+                                인기 있어지면 결국 프랜차이즈들이 들어오고, 동네들이 획일적으로 바뀌는 느낌?<br />
+                                그래서 요즘은 잘 알려지지 않은, 고유의 맛과 멋이 있는 지역들이 더 특색 있다고 느껴지는 것 같아요."
+                            </p>
+                            <p className='InterviewSource'>
+                                - 로컬 라이프스타일 매거진 &lt;로컬키트&gt;와의 인터뷰 중<br />
+                                (@local.kit_official)
+                            </p>
+                        </div>
+                    </div>
                 </div>
-            </FeatureContainer>
-            <TargetedContainer>
-                <TargetTitle>???겟별 ?���? ?���?</TargetTitle>
-                <TargetCreater>
-                    <img src={mockup3} alt="Mockup 3" />
-                    <p>
-                        Self-improvement is a journey of setting and achieving goals. In this blog post, we introduce
-                        five key habits that can be easily integrated into daily life. The first is goal setting and
-                        time management, which lay the foundation for personal achievement and professional growth. The
-                        second habit is self-encouragement through positive thinking, which is crucial for overcoming
-                        challenges and moving towards success. The third is maintaining health through regular exercise
-                        and a balanced diet.
-                    </p>
-                </TargetCreater>
-                <TargetConsumer>
-                    <p>
-                        Self-improvement is a journey of setting and achieving goals. In this blog post, we introduce
-                        five key habits that can be easily integrated into daily life. The first is goal setting and
-                        time management, which lay the foundation for personal achievement and professional growth. The
-                        second habit is self-encouragement through positive thinking, which is crucial for overcoming
-                        challenges and moving towards success. The third is maintaining health through regular exercise
-                        and a balanced diet.
-                    </p>
-                    <img src={mockup4} alt="Mockup 4" />
-                </TargetConsumer>
-            </TargetedContainer>
+                <div className='BrandCover'>
+                    <div className='TextBox'>
+                        <div className='BrandNameBox'>
+                            <p className='BrandName'>순솝</p>
+                            <p className='BrandLocation'>전라남도 순천시의 로컬 비누 브랜드</p>
+                        </div>
+                        <div className='InterviewBox'>
+                            <p className='Interview'>
+                                순솝은 키트 안에 순천의 스토리를 담았다. 소비자는 체험을 통해 비누를 제작하며<br />
+                                갈대, 겨울 철새, 수달 등 순천만과 동천에 사는 동식물 이야기를 들을 수 있다. 순솝 비누 제작 키트를<br />
+                                통해 소비자는 순천만의 이야기와 비누를 제작하는 과정 자체의 친환경성을 경험할 수 있다. ...<br />
+                                "밖에 나갔다가 다시 오니까 순천이 조금 달라 보였어요."
+                            </p>
+                            <p className='InterviewSource'>
+                                - 로컬 라이프스타일 매거진 &lt;로컬키트&gt;와의 인터뷰 중<br />
+                                (@local.kit_official)
+                            </p>
+                        </div>
+                    </div>
+                    <div className='ImageBox'>
+                        <img src={mockup4} alt='brand_cover_2' />
+                    </div>
+                </div>
+            </BrandCoverContainer>
         </LandingContainer>
     );
 }
@@ -144,6 +156,10 @@ const LandingContainer = styled.div`
     flex-direction: column;
     align-items: center;
     padding-bottom: 20px;
+
+    .ContainerBox {
+        background-color: #F5F5F5;
+    }
 `;
 
 const Highlight = styled.span`
@@ -153,11 +169,14 @@ const Highlight = styled.span`
 const CreatorContainer = styled.div`
     display: flex;
     width: 100vw;
-    height: 43vh;
-    padding: 20px;
+    height: auto;
+    padding: 40px;
 
     .TextContainer {
         flex: 0 0 25%;
+        display: flex; 
+        flex-direction: column;
+        justify-content: space-between; 
         align-items: flex-start;
 
         button {
@@ -180,10 +199,25 @@ const CreatorContainer = styled.div`
         display: flex;
         flex-direction: row;
         flex: 0 0 75%;
+        flex-wrap: wrap;
+        width: 75%;
+        align-content: space-around;
 
         .ImageBox {
+            flex: 0 0 33%;
+            padding-bottom: 25%;
+            position: relative;
             display: flex;
             flex-direction: column;
+
+            img{
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+            }
 
             .caption {
                 .explain {
@@ -200,12 +234,34 @@ const CreatorContainer = styled.div`
 
 const ConsumerContainer = styled.div`
     width: 100vw;
-    height: 43vh;
-    padding: 20px;
+    height: auto;
+    padding: 40px;
+    display: flex;
+    flex-direction: row;
 
-    .ImageBox {
+    .ImageBoxContainer {
+        display: flex;
         flex-direction: row;
+        flex-wrap: wrap;
         flex: 0 0 50%;
+        width: 100%;
+        align-content: space-around;
+
+        .ImageBox {
+            flex: 0 0 50%; 
+            padding-bottom: 40%; 
+            position: relative;
+            display: flex;
+
+            img {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                object-fit: contain;
+            }
+        }
     }
     
     h4 {
@@ -215,108 +271,69 @@ const ConsumerContainer = styled.div`
 
     .TextBox {
         flex: 0 0 50%;
+        display: flex;
         flex-direction: column;
+        justify-content: space-between;
         align-items: flex-end;
+    }
 
-        button {
-            background-color: #FF8162;
-            color: white;
-            border: none;
-            padding: 10px 40px;
-            cursor: pointer;
-            font-size: 1.5em;
-            border-radius: 5px;
-        }
+    button {
+        background-color: #FF8162;
+        color: white;
+        border: none;
+        padding: 10px 40px;
+        cursor: pointer;
+        font-size: 1.5em;
+        border-radius: 5px;
     }
 `;
 
-const FeatureContainer = styled.div`
-    display: flex;
-    align-items: flex-start;
-    margin-top: 100px;
-    margin-bottom: 100px;
-    width: 80%;
-
-    #feature-title {
-        font-size: 2em;
-        font-weight: bold;
-    }
-
-    img {
-        margin-right: 30px;
-        width: 30%;
-    }
-
-    div {
-        align-items: flex-end;
-        flex: 1;
-        text-align: right;
-    }
-
-    h4 {
-        margin-bottom: 10px;
-    }
-
-    p {
-        margin-bottom: 20px;
-    }
-
-    ul {
-        list-style-type: disc;
-        padding-right: 20px;
-        text-align: right;
-        margin-bottom: 20px;
-    }
-
-    li {
-        text-align: right;
-    }
-`;
-
-const TargetedContainer = styled.div`
+const BrandCoverContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-bottom: 100px;
-`;
+    padding: 40px;
 
-const TargetTitle = styled.h4`
-    margin-bottom: 50px;
+    .BrandCover {
+        display: flex;
+        flex-direction: row;
+        margin-top: 40px;
+        margin-bottom: 40px;
 
-    font-size: 2em;
-    font-weight: bold;
-`;
+        .ImageBox {
+            flex: 0 0 33%;
+        }
 
-const TargetCreater = styled.div`
-    display: flex;
-    align-items: center;
-    margin-bottom: 60px;
-    text-align: left;
-    width: 80%;
+        .TextBox {
+            flex: 0 0 66%;
+            padding: 30px;
 
-    img {
-        margin-right: 20px;
-        width: 30%;
+            .BrandNameBox {
+                margin-bottom: 15px;
+                .BrandName{
+                    font-size: 1.5em;
+                    font-weight: bold;
+                }
+                .BrandLocation{
+                    font-size: 0.75em;
+                    color: #4b5563;
+                }
+            }
+
+            .InterviewBox {
+                display: flex;
+                flex-direction: column;
+                align-items: flex-end;
+
+                .Interview{
+                    font-size: 1em;
+                    align-self: flex-start;
+                }
+                .InterviewSource{
+                    font-size: 0.75em;
+                    color: #6b7280;
+                }
+            }
+        }
     }
-
-    p {
-        flex: 1;
-    }
-`;
-
-const TargetConsumer = styled.div`
-    display: flex;
-    align-items: center;
-    margin-botton: 60px;
-    text-align: left;
-    width: 80%;
-
-    img {
-        margin-left: 20px;
-        width: 30%;
-    }
-
-    p {
-        flex: 1;
-    }
-`;
+`
