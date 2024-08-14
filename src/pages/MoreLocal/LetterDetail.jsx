76ad78d2857651328b57f79131922f1b4877bea8
triangle-park <silver3garden@gmail.com>
2024-08-14 15:50:32 +0900

    레터디테일 기초

--- src/pages/MoreLocal/LetterDetail.jsx
+++ src/pages/MoreLocal/LetterDetail.jsx
@@ -2,7 +2,10 @@ import React, { useState, useEffect, useContext } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
-const LetterDetail = () => {
+const LetterDetail = ({letters}) => {
+    const { id } = useParams();
+    const letter = letters.find(letter => letter.id === parseInt(id));
+
     return (
 
     );
