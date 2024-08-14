78617e2fc920c567b1ee8495f093b06dee702ee6
triangle-park <silver3garden@gmail.com>
2024-08-14 15:24:35 +0900

    레터 디테일 파일 생성

---
+++ src/pages/MoreLocal/LetterDetail.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const LetterDetail = () => {
+    return (
+
+    );
+};
+
+export default LetterDetail;
